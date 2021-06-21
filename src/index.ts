33ae76254b39cbefa8e19c0126b15911b0dc465b
Will Garrett <willgarrett610@gmail.com>
2021-06-20 22:29:42 -0600

    Canvas resizing

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,29 @@ import { Canvas, CanvasKit, Surface } from "canvaskit-wasm";
 (CanvasKitInit as any)({
     locateFile: (file: string) => file,
 }).then((CK: CanvasKit) => {
-    const surface: Surface | null = CK.MakeCanvasSurface("main");
-    if (!surface) return;
-    const surf: any = surface;
+    let surface: Surface | null = null;
+    let surf: any = surface;
+
+    function setupSurface() {
+        const heightOutput = window.innerHeight;
+        const widthOutput = window.innerWidth;
+
+        console.log(heightOutput);
+
+        let canvasElmt:any = document.getElementById("main");
+        canvasElmt.width = widthOutput;
+        canvasElmt.height = heightOutput;
+
+        surface = CK.MakeCanvasSurface("main");
+        surf = surface;
+    }
+
+    setupSurface();
+
+    window.onresize = e => {
+        setupSurface();
+    };
+
     const paint = new CK.Paint();
     paint.setColor(CK.Color4f(0.9, 0, 0, 1.0));
     paint.setStyle(CK.PaintStyle.Stroke);
@@ -20,12 +40,12 @@ import { Canvas, CanvasKit, Surface } from "canvaskit-wasm";
     let dirX = 1; // box is always moving at a constant speed in one of the four diagonal directions
     let dirY = 1;
 
-    function drawFrame(canvas) {
+    function drawFrame(canvas:Canvas) {
         // boundary check
-        if (x < 0 || x + w > 300) {
+        if (x < 0 || x + w > window.innerWidth) {
             dirX *= -1; // reverse x direction when hitting side walls
         }
-        if (y < 0 || y + h > 300) {
+        if (y < 0 || y + h > window.innerHeight) {
             dirY *= -1; // reverse y direction when hitting top and bottom walls
         }
         // move
