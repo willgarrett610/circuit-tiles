18fb6c1239963304516f70796af85fa73d5bc570
Will Garrett <willgarrett610@gmail.com>
2021-07-11 20:33:04 -0600

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
     mode: "production",
