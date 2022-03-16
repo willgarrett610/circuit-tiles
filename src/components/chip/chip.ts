af19a0147fdebc8216659a634135f7b0cf52f464
Justin Fernald <jfernald@my.gcu.edu>
2022-03-15 22:32:06 -0700

    fixed for chip highlight  / chip in chip

--- src/components/chip/chip.ts
+++ src/components/chip/chip.ts
@@ -39,6 +39,8 @@ export class Chip {
 
     chipDependencies = new Set<Chip>();
 
+    placedChipsInside: PlacedChip[] = [];
+
     /**
      * Chip constructor
      *
--- src/components/grid/interactive_chip_grid.ts
+++ src/components/grid/interactive_chip_grid.ts
@@ -15,14 +15,17 @@ export default class InteractiveChipGrid extends InteractiveGrid {
      * @param chip the chip the grid is for
      * @param size pixel size of grid tile
      * @param tiles initial tiles
+     * @param placedChips initial placed chips
      */
     constructor(
         chip: Chip,
         size: number,
-        tiles?: { [key: string]: Tile | undefined }
+        tiles?: { [key: string]: Tile | undefined },
+        placedChips?: PlacedChip[]
     ) {
         super(size, tiles);
         this.chip = chip;
+        if (placedChips) this.chips = placedChips;
     }
 
     /**
@@ -39,7 +42,14 @@ export default class InteractiveChipGrid extends InteractiveGrid {
         recordHistory = true,
         injectPlaceChip: PlacedChip | undefined = undefined
     ) {
-        if (originalChip.getRootOriginal().chipDependencies.has(this.chip)) {
+        console.log(
+            [...originalChip.getRootOriginal().getChipDependencies()].map(
+                (x) => x.name
+            )
+        );
+        if (
+            originalChip.getRootOriginal().getChipDependencies().has(this.chip)
+        ) {
             console.log("oh no! you can't do that!");
             return;
         }
