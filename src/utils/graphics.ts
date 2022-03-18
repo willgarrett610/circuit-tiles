aa7f57e310127c0dc0ec29ff32bf532a3d0a4641
Justin Fernald <jfernald@my.gcu.edu>
2022-03-18 11:26:20 -0600

    doc update

--- src/utils/graphics.ts
+++ src/utils/graphics.ts
@@ -14,6 +14,7 @@ import { clamp } from "./math";
  * @param radius.topRight
  * @param radius.botRight
  * @param radius.botLeft
+ * @param graphicUpdater
  * @returns Pixi container
  */
 export function generateRoundedRectContainer(
